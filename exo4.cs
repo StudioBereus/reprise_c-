ddb6024a1e25872f4f12433e9122a2052c720aab
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2022-12-19 11:42:07 +0100

    Add files via upload

---
+++ exo4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans, num1, num2, chang;
+            Console.WriteLine("Entrez deux chiffres");
+            ans = Console.ReadLine();
+            num1 = Convert.ToString(ans[0]);
+            num2 = Convert.ToString(ans[2]);
+
+            chang = num1;
+            num1 = num2;
+            num2 = chang;
+
+            Console.WriteLine(num1 + " et " + num2);
+
+        }
+    }
+}
